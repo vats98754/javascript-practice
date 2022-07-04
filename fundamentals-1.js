a0f66a1774b518a842ab2713adad8287a0599467
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-04 16:15:54 +1000

    Renamed 'script.js' to 'fundamentals-1.js'

---
+++ fundamentals-1.js
@@ -0,0 +1,15 @@
+// Logging basic arithmetic on the JS console
+console.log(43 + 26);
+console.log(43 + 26 + 120 + 30 + 100 + 101);
+console.log((4 + 6 + 9) / 77);
+
+// Using Variables
+let a = 10;
+console.log(a);
+console.log(9*a);
+let b = 7*a;
+console.log(b);
+
+let max = 57;
+let actual = max - 13;
+let percentage = actual/max;
