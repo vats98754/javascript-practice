2de16336ab3dd134990ccb8319c7b2f90f5f99fb
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-10 06:55:57 +0800

    Farenheit to Celcius works on all tests Had to tinker with Math.round(10 * expression)/10 to get it working

--- jest-testing/07_tempConversion/tempConversion.js
+++ jest-testing/07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(faren) {
+  return Math.round(10 * (faren - 32) * 5 / 9) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celc) {
+  return Math.round(10 * (celc * 9 / 5) + 32) / 10;
 };
 
 // Do not edit below this line
