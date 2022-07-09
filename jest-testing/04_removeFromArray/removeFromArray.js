dd9ca7227f798d590ba7d990bc5b975dd7e79e2f
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-10 00:40:27 +0800

    Use arguments (Array-like object) for n parameters; Pass all tests Since arguments.slice() did not work as Array methods do not work on it, removeArr was declared using a for loop iterating through arguments starting from index 1.

--- jest-testing/04_removeFromArray/removeFromArray.js
+++ jest-testing/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
-
+    const arr = arguments[0];
+    const removeArr = [];
+    for (let i = 1; i < arguments.length; i++) {
+        removeArr.push(arguments[i]);
+    }
+    removeArr.forEach((itemToBeRemoved) => {
+        arr.forEach((item) => {
+            if (item === itemToBeRemoved) {
+                arr.splice(arr.indexOf(item), 1);
+            }
+        });
+    });
+    return arr;
 };
 
 // Do not edit below this line
