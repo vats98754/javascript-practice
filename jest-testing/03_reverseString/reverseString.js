765078213dd8e51a2a402a60b8f0581014fd5d90
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-09 23:38:19 +0800

    Write reverseString() and pass all tests in one go Set str as input parameter and returned reversed string using while loop

--- jest-testing/03_reverseString/reverseString.js
+++ jest-testing/03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strReversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        strReversed += str[i];
+    }
+    return strReversed;
 };
 
 // Do not edit below this line
