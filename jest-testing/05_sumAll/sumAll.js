db505bfd27eb386f9e08ff78b2665946b08028d7
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-10 01:51:07 +0800

    Use formula (not loop) to sum consecutive numbers within the input range Returned appropriate error messages for:     - interchanged start and end values of input range,     - negative number inputs for the range, and     - non-numeric inputs for the range.

--- jest-testing/05_sumAll/sumAll.js
+++ jest-testing/05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    // Fixing if end is less than start by reassigning start to min(start,end) and end to max(start,end)
+    const first = Math.min(start, end);
+    const last = Math.max(start, end);
+    // Fixing if either number is negative; fixing if either parameter is not a number
+    if (first < 0 || typeof start !== 'number' || typeof end !== 'number') {
+        return 'ERROR';
+    }
+    return (first + last) * (last - first + 1) / 2;
 };
 
 // Do not edit below this line
