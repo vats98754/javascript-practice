750662d7c017e4a73a083274fa96e405d0630534
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-10 06:58:08 +0800

    Celcius to Farenheit works on all tests First test failed initially as parentheses were missing around the exp:     (celc * 9 / 5) + 32 Which was to then be multiplied by 10, within Math.round() / 10.

--- jest-testing/07_tempConversion/tempConversion.js
+++ jest-testing/07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const ftoc = function(faren) {
-  return Math.round(10 * (faren - 32) * 5 / 9) / 10;
+  return (Math.round(10 * (faren - 32) * 5 / 9) / 10);
 };
 
 const ctof = function(celc) {
-  return Math.round(10 * (celc * 9 / 5) + 32) / 10;
+  return (Math.round(10 * ((celc * 9 / 5) + 32)) / 10);
 };
 
 // Do not edit below this line
