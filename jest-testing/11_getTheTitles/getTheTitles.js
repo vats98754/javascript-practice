2089047fe5ef09fd31f7410d92c9a4c93604c674
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-10 22:35:24 +0800

    Get titles by iterating through Array of objects using 'title' attribute

--- jest-testing/11_getTheTitles/getTheTitles.js
+++ jest-testing/11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    const bookTitles = [];
+    for (let i = 0; i < arr.length; i++) {
+        bookTitles.push(arr[i].title);
+    }
+    return bookTitles;
 };
 
 // Do not edit below this line
