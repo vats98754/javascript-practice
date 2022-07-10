a21ac238d45dc10bc0978541e6671f06b95713de
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-11 06:27:59 +0800

    Learned how to work around non-existent object attributes Passed all tests after tedious research on Proxies/JS objects.

--- jest-testing/12_findTheOldest/findTheOldest.js
+++ jest-testing/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    if (!oldest.hasOwnProperty('yearOfDeath')) {
+        let currentTime = new Date();
+        oldest['yearOfDeath'] = currentTime.getFullYear();
+    }
+    for (let i = 1; i < people.length; ++i) {
+        let copy = people[i];
+        if (!copy.hasOwnProperty('yearOfDeath')) {
+            let currentTime = new Date();
+            copy['yearOfDeath'] = currentTime.getFullYear();
+        }
+        if (copy.yearOfDeath-copy.yearOfBirth > oldest.yearOfDeath-oldest.yearOfBirth) {
+            oldest = people[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
