660fb84146fb4c5c2aa5343de8b809eec85ff7d8
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-10 15:40:48 +0800

    Use 'calculator.spec.js' to determine functions' parameters All 16 tests passed. Functions were written sequentially (they were tested right after being written to debug them instantly, though no debugging was necessary as the functions passed all tests in one go).

--- jest-testing/08_calculator/calculator.js
+++ jest-testing/08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return (a - b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  arr.forEach((num) => {
+    sum += num;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mult = 1;
+  arr.forEach((num) => {
+    mult *= num;
+  });
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return (a ** b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+  for (let i = 2; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
