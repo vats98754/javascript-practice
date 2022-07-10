b45f85ec27f8dda9dffc1dfba27ef8584ee86e84
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-10 16:53:57 +0800

    Use loop to middle of filtered string to compare against its reflection Use REGEX to remove punctuation and all whitespaces to filter string

--- jest-testing/09_palindromes/palindromes.js
+++ jest-testing/09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let flag = true;
+    // Using regex to remove punctuation and all whitespace; Converts all characters to lowercase
+    const strCopy = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replaceAll(/\s/g,'').toLowerCase();
+    const len = strCopy.length;
+    for (let i = 0; i < len; i++) {
+        if (strCopy[i] !== strCopy[len - 1 - i]) {
+            flag = false;
+        }
+    }
+    return flag;
 };
 
 // Do not edit below this line
