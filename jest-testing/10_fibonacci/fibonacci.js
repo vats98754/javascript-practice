e0535c3f1930232bf7f6eec725c45cc1318474d2
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-10 17:13:28 +0800

    Use memoization to speed up the fibonacci function Alternatively, Binet's formula can be used for more speed. Failed test 6, which returns 'OOPS' if input index is negative. Algorithm automatically works with strings like "8", so there was no need to adjust the code to pass those tests.

--- jest-testing/10_fibonacci/fibonacci.js
+++ jest-testing/10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    // If given index is negative, return "OOPS"
+    if (index < 0) {
+        return "OOPS";
+    }
+    const arrFib = [0, 1];
+    for (let i = 2; i <= index; i++) {
+        arrFib.push(arrFib[i - 2] + arrFib[i - 1]);
+    }
+    return arrFib[index];
 };
 
 // Do not edit below this line
