bad819d8007670916ce1bee634ecc83910edc045
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-11 06:52:16 +0800

    Implement Regex to filter alphabets; All tests passed once Regex correct Used modulus operator and avoided negative mod values to 'wrap around' when shifting chars. Different conditionals used for upper and lowercase chars to ensure the 'base' char values after subtraction from unicode values had range 0-26.

--- jest-testing/13_caesar/caesar.js
+++ jest-testing/13_caesar/caesar.js
@@ -1,5 +1,17 @@
-const caesar = function() {
-
+const caesar = function(str, shift) {
+    let retStr = '';
+    for (let i = 0; i < str.length; ++i) {
+        let char = str[i];
+        if (char.match("^[a-z\(\)]+$")) {
+            // extra +26 added withing .fromCharCode() to make negative mod values positive
+            char = String.fromCharCode((char.charCodeAt(0) - 97 + shift + 26) % 26 + 97);
+        } else if (char.match("^[A-Z\(\)]+$")) {
+            // extra +26 added withing .fromCharCode() to make negative mod values positive
+            char = String.fromCharCode((char.charCodeAt(0) - 65 + shift + 26) % 26 + 65);
+        }
+        retStr += char;
+    }
+    return retStr;
 };
 
 // Do not edit below this line
